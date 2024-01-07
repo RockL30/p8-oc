77d3aac4922cdfdc1ae2f98fba08fc1bfa5faef2
Francisco <mateus.jp.francisco@gmail.com>
2024-01-07 10:38:45 +0100

    Fixed test

--- test/ControllerSpec.js
+++ test/ControllerSpec.js
@@ -286,10 +286,14 @@ describe('controller', function () {
 
 	describe('element removal', function () {
 		it('should remove an entry from the model', function () {
+			// TODO: write test
+			// mock model with a single todo
 			var todo = { id: 42, title: 'my todo', completed: true };
+			// set up model with the mock model
 			setUpModel([todo]);
-
+			// set the view to an empty string
 			subject.setView('');
+			// trigger the itemRemove event with the id of the todo
 			view.trigger('itemRemove', { id: 42 });
 
 			expect(model.remove).toHaveBeenCalledWith(42, jasmine.any(Function));
